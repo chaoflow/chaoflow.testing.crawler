4a65624227b335229a0c9e7b3a7a2c7cea569559
chaoflow <chaoflow@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2008-11-08 05:22:12 +0000

    paula: preparing paula.app.testing
    git-svn-id: https://svn.plone.org/svn/collective/paula/trunk@75289 db7f04ef-aaf3-0310-a811-c281ed44c4ad

---
+++ paula.app.testing/README.txt
@@ -0,0 +1,5 @@
+Introduction
+============
+
+paula.testing provides get_test_suite, that searches your package recursively,
+finds all python files and registers contained unit tests for testing.
---
+++ paula.app.testing/src/paula/testing/module.py
@@ -0,0 +1,36 @@
+# Copyright (c) 2008 by Florian Friesdorf
+#
+# GNU Affero General Public License (AGPL)
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation; either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License along with this program.  If not, see
+# <http://www.gnu.org/licenses/>.
+"""
+"""
+__author__ = "Florian Friesdorf <flo@chaoflow.net>"
+__docformat__ = "plaintext"
+
+from zope.interface import implements, Interface
+
+class ISomeObjectUtility(Interface):
+    pass
+
+class SomeObject(object):
+    """
+    This is an example test, that just should be found by paula.testing
+
+        >>> 1 + 1
+        2
+    """
+    implements(ISomeObjectUtility)
+    name = 'some object utility'
