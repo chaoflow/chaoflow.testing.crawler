5b7244b883a0181501d804d37f731222c5325ff2
Florian Friesdorf <flo@chaoflow.net>
2009-11-01 18:10:16 +0100

    fixed files to reflect new license: LGPL

--- src/chaoflow/testing/crawler/globs.py
+++ src/chaoflow/testing/crawler/globs.py
@@ -1,20 +1,21 @@
 # Copyright (c) 2008-2009 by Florian Friesdorf
 #
-# GNU Affero General Public License (AGPL)
+# Lesser General Public License (LGPL)
 #
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation; either version 3 of the
-# License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
+# This file is part of chaoflow.testing.crawler.
+# 
+# chaoflow.testing.crawler is free software: you can redistribute it and/or
+# modify it under the terms of the Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+# 
+# chaoflow.testing.crawler is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public
-# License along with this program.  If not, see
-# <http://www.gnu.org/licenses/>.
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+# Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along with
+# chaoflow.testing.crawler.  If not, see <http://www.gnu.org/licenses/>.
 
 __author__ = "Florian Friesdorf <flo@chaoflow.net>"
 __docformat__ = "plaintext"
